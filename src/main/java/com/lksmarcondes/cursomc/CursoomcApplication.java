6fe5deac8fedda4e5dff3f602dbd55ec4a395f80
Lucas Henrique <lucashenrique125@hotmail.com>
2020-12-04 00:09:05 -0400

    Projeto Criado

---
+++ src/main/java/com/lksmarcondes/cursomc/CursoomcApplication.java
@@ -0,0 +1,13 @@
+package com.lksmarcondes.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoomcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lksmarcondes/cursomc/CursoomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lksmarcondes.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
